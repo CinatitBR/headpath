24d7558f5d8987bbe88e12713237763dabaa7e55
CinatitBR <cinatit.igor.santos@gmail.com>
2021-01-29 21:46:13 -0300

    Store focus state of digits

--- frontend/src/components/InputDuration/index.jsx
+++ frontend/src/components/InputDuration/index.jsx
@@ -1,16 +1,59 @@
+import { useState } from 'react'
+
 import './style.css'
 
 const InputDuration = () => {
-  
+  const defaultFocusState = [false, false, false, false]
+
+  const [digitFocus, setDigitFocus] = useState(defaultFocusState)
+
+  function handleDigitFocus(e) {
+    const digitIndex = e.target.dataset.index
+
+    const newFocusState = [...defaultFocusState]
+    newFocusState[digitIndex] = true
+
+    setDigitFocus(newFocusState)
+  }
+
   return (
     <div className="input-duration">
       <div>
-        <span className="digit selected">0</span>
-        <span className="digit">0</span>
+        <span 
+          className={`digit ${digitFocus[3] && 'cursor'}`} 
+          tabIndex="0"
+          data-index="3"
+          onFocus={handleDigitFocus}
+        >
+          0
+        </span>
+
+        <span 
+          className={`digit ${digitFocus[2] && 'cursor'}`} 
+          tabIndex="0"
+          data-index="2"
+          onFocus={handleDigitFocus}
+        >
+          0
+        </span>
         <span className="symbol">h</span>
 
-        <span className="digit">0</span>
-        <span className="digit">0</span>
+        <span 
+          className={`digit ${digitFocus[1] && 'cursor'}`} 
+          tabIndex="0"
+          data-index="1"
+          onFocus={handleDigitFocus}
+        >
+          0
+        </span>
+        <span 
+          className={`digit ${digitFocus[0] && 'cursor'}`} 
+          tabIndex="0"
+          data-index="0"
+          onFocus={handleDigitFocus}
+        >
+          0
+        </span>
         <span className="symbol">m</span>
       </div>
 
