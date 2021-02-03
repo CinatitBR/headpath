546dc03d830a31119bee07ec55052fffc0f1edc5
CinatitBR <cinatit.igor.santos@gmail.com>
2021-02-03 15:51:06 -0300

    Refactor code

--- backend/src/Controllers/SubjectController.js
+++ backend/src/Controllers/SubjectController.js
@@ -3,58 +3,58 @@ import SubjectModel from '../Models/SubjectModel.js'
 import validator from '../services/validator.js'
 import timeHelper from '../services/timeHelper.js'
 
-const SubjectController = {
-  index: async (req, res, next) => {
-    try {
-      const subjects = await Subject.findAll()
-  
-      return res.json(subjects)
-    }
-    catch (err) { next(err) }
-  },
+const index = async (req, res, next) => {
+  try {
+    const subjects = await SubjectModel.findAll()
 
-  create: async (req, res, next) => {
-    try {
-      const { subject, duration } = req.body
-
-      const success = { 
-        status: 'ok', 
-        feedback: 'Matéria adicionada com sucesso!' 
-      }
-
-      const errors = {
-        status: 'error',
-        subject: null,
-        duration: null
-      }
-
-      errors.subject = await validator.subject({ subject, SubjectModel })
-      errors.duration = validator.duration({ duration })
-
-      const errorsExist = (errors.subject || errors.duration)
-
-      // If there is any error
-      if (errorsExist) {
-        return res
-          .status(400)
-          .json(errors)
-      }
-
-      // Formats duration so that minutes and seconds have 59 units at most
-      const newDuration = timeHelper.formatTime(duration)
-
-      // Creates subject
-      await SubjectModel.create({ 
-        subject: subject, 
-        duration: newDuration 
-      })
-
-      return res
-        .status(201)
-        .json(success)
-    }
-    catch (err) { next(err) }
+    return res.json(subjects)
   }
+  catch (err) { next(err) }
 }
+
+const create = async (req, res, next) => {
+  try {
+    const { subject, duration } = req.body
+
+    const success = { 
+      status: 'ok', 
+      feedback: 'Matéria adicionada com sucesso!' 
+    }
+
+    const errors = {
+      status: 'error',
+      subject: null,
+      duration: null
+    }
+
+    errors.subject = await validator.subject({ subject, SubjectModel })
+    errors.duration = validator.duration({ duration })
+
+    const errorsExist = (errors.subject || errors.duration)
+
+    // If there is any error
+    if (errorsExist) {
+      return res
+        .status(400)
+        .json(errors)
+    }
+
+    // Formats duration so that minutes and seconds have 59 units at most
+    const newDuration = timeHelper.formatTime(duration)
+
+    // Creates subject
+    await SubjectModel.create({ 
+      subject: subject, 
+      duration: newDuration 
+    })
+
+    return res
+      .status(201)
+      .json(success)
+  }
+  catch (err) { next(err) }
+}
+
+const SubjectController = { index, create }
 
 export default SubjectController
