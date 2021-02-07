27f8e713a017fb2c7d09de2204c29145865bc4b6
CinatitBR <cinatit.igor.santos@gmail.com>
2021-02-07 13:41:20 -0300

    Schedule setInterval to update milliseconds left

--- frontend/src/components/CurrentSubjectTimer/index.jsx
+++ frontend/src/components/CurrentSubjectTimer/index.jsx
@@ -2,36 +2,49 @@ import { useState } from 'react'
 
 import TimerDisplay from '../TimerDisplay'
 import CurrentSubject from '../CurrentSubject'
-import TimerControls from '../TimerControls' 
+import TimerControls from '../TimerControls'
 
+import timeHelper from '../../services/timeHelper'
 import style from './style.module.css'
 
-const defaultCurrentSubject = {
-  subject: null, 
-  duration: null
-}
-
-const CurrentSubjectTimer = ({ currentSubject = defaultCurrentSubject }) => {
+const CurrentSubjectTimer = ({ currentSubject }) => {
   const { subject, duration } = currentSubject
 
-  const start = () => {
-    const startTime = Date.now()
-    const endTime = startTime
+  const [timerId, setTimerId] = useState(null)
+  const [millisecondsLeft, setMillisecondsLeft] = useState(
+    timeHelper.fromTimeToMilliseconds(duration)
+  )
 
+  const formatedMilliseconds = 
+    timeHelper.fromMillisecondsToTime(millisecondsLeft)
+
+  const start = () => {
+    if (timerId) return
+
+    const newTimerId = setInterval(() => {
+      setMillisecondsLeft(prevmillisecondsLeft => 
+        prevmillisecondsLeft - 1000
+      )
+    }, 1000)
+
+    setTimerId(newTimerId)
   }
 
-  if (!(subject && duration)) return null
+  const stop = () => {
+    if (!timerId) return
+
+    clearInterval(timerId)
+  }
+
+  if (!subject && !duration) return null
 
   return (
     <section className={style.currentSubjectTimer}>
-      <TimerDisplay duration={duration} />
+      <TimerDisplay time={formatedMilliseconds} />
 
       <CurrentSubject subject={subject} />
 
-      <TimerControls 
-        onStartTimer={start} 
-        // onStopTimer={stopTimer} 
-      />
+      <TimerControls onStartTimer={start} onStopTimer={stop} />
     </section>
   )
 }
