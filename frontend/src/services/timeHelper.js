d0fdf71cdd5546b280052b2505b4587c8825c90d
CinatitBR <cinatit.igor.santos@gmail.com>
2021-02-07 14:08:11 -0300

    Create function to convert milliseconds to time

--- frontend/src/services/timeHelper.js
+++ frontend/src/services/timeHelper.js
@@ -29,6 +29,36 @@ const fromTimeToMilliseconds = time => {
   return milliseconds
 }
 
-const timeHelper = { formatFormDuration, fromTimeToMilliseconds }
+const fromMillisecondsToTime = milliseconds => {
+  // Calculate time units
+  let seconds = Math.floor(milliseconds / 1000)
+
+  let minutes = Math.floor(seconds / 60)
+  seconds = seconds % 60
+
+  let hours = Math.floor(minutes / 60)
+  minutes = minutes % 60
+
+  // Format to time
+  if (seconds < 10) {
+    seconds = '0' + seconds
+  }
+  if (minutes < 10) {
+    minutes = '0' + minutes
+  }
+  if (hours < 10) {
+    hours = '0' + hours
+  } 
+
+  const time = `${hours}:${minutes}:${seconds}`
+
+  return time
+}
+
+const timeHelper = { 
+  formatFormDuration, 
+  fromTimeToMilliseconds,
+  fromMillisecondsToTime 
+}
 
 export default timeHelper
