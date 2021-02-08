3de8f180d39f173f4fbb7ee65825c20f5f99ca32
CinatitBR <cinatit.igor.santos@gmail.com>
2021-02-08 16:00:41 -0300

    Change display color based on timer state

--- frontend/src/components/TimerDisplay/index.jsx
+++ frontend/src/components/TimerDisplay/index.jsx
@@ -1,8 +1,8 @@
 import style from './style.module.css'
 
-const TimerDisplay = ({ time }) => {
+const TimerDisplay = ({ time, isTimerRunning }) => {
   return (
-    <div className={style.timerDisplay}>
+    <div className={`${style.timerDisplay} ${isTimerRunning ? style.running : ''}`}>
       {time}
     </div>
   )
