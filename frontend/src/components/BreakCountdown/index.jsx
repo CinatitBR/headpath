95f5640548d92f819cee2ad83031030b3f73c16e
CinatitBR <cinatit.igor.santos@gmail.com>
2021-02-21 18:45:22 -0300

    Update style

--- frontend/src/components/BreakCountdown/index.jsx
+++ frontend/src/components/BreakCountdown/index.jsx
@@ -6,17 +6,17 @@ import style from './style.module.css'
 const BreakCountdown = () => {
   return (
     <div className={style.breakCountdown}>
-      <ProgressRing 
-        radius={25}
-        strokeWidth={5}
-        progress={50}
-        progressColor="#03a9f4"
-        pathColor="#E1E1E1"
-      >
-        <button type="button">
-          <FaMugHot size={'25px'} color={'var(--blue)'} />
-        </button>
-      </ProgressRing>
+      <button type="button">
+        <ProgressRing 
+          radius={25}
+          strokeWidth={5}
+          progress={50}
+          progressColor="var(--brown)"
+          pathColor="var(--grey)"
+        >
+          <FaMugHot size={'25px'} color={'var(--brown)'} />
+        </ProgressRing>
+      </button>
     </div>
   )
 }
