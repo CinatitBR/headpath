93d3b3a8187436317aacb0313211d902c80169bb
Igor Rocha <cinatit.igor.santos@gmail.com>
2021-05-08 15:06:15 -0300

    Update BreakMenu component

--- frontend/src/components/BreakMenu/index.jsx
+++ frontend/src/components/BreakMenu/index.jsx
@@ -2,9 +2,37 @@ import style from './style.module.css'
 
 const BreakMenu = () => {
   return (
-    <div>
-      <h1>testing</h1>
-    </div>
+    <section className={style.BreakMenu}>
+      <form>
+        <header>
+          <h3 className={style.title}>Intervalo</h3>
+        </header>
+
+        <div className={style.fields}>
+
+          <div className={style.field}>
+            <label>
+              Frequência
+
+              <span>25m</span>
+            </label>
+          </div>
+
+          <div className={style.field}>
+            <label>
+              Duração
+
+              <span>5m</span>
+            </label>
+          </div>
+
+        </div>
+
+        <button type="button" className={style.editButton}>
+          Edit
+        </button>
+      </form>
+    </section>
   )
 }
 
