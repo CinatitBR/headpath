9ba088110720dd254fbe5d0db5a8bfda714db3bb
Igor Rocha <cinatit.igor.santos@gmail.com>
2021-05-08 15:10:38 -0300

    Add cors middleware

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,12 @@
 import express from 'express'
+import cors from 'cors'
 import router from './routes.js'
 
 import ErrorController from './Controllers/ErrorController.js'
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(router)
 app.use(ErrorController.index)
